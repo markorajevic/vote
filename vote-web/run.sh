25d7bf484feb74d8bdd04675f2353185329c04b7
Marko Rajevic <me@markorajevic.space>
2019-01-21 19:55:58 +0100

    run.sh updated
    Signed-off-by: Marko Rajevic <me@markorajevic.space>

--- vote-web/run.sh
+++ vote-web/run.sh
@@ -3,7 +3,7 @@
 npm install
 npm run build
 pm2 delete vote9601
-pm2 start --name "vote9601" npm -- run startrbln
+pm2 start --name "vote9601" npm -- run start
 sudo cp nginx/vote.avocode.rs /etc/nginx/sites-available/vote.avocode.rs
 sudo ln -s -f /etc/nginx/sites-available/vote.avocode.rs /etc/nginx/sites-enabled/
 sudo nginx -t
